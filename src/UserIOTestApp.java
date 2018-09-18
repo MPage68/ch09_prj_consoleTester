d0dca86e3e1571a661269fd9d3b76718ff495ccf
mpage68 <mikepage68@live.com>
2018-09-17 21:27:43 -0400

    update

--- src/UserIOTestApp.java
+++ src/UserIOTestApp.java
@@ -3,27 +3,22 @@ public class UserIOTestApp {
 
 	public static void main(String[] args) {
 
-		// establish console
-		ConsoleIO console = IOFactory.getUserIO();
-		// Welcome message
-		console.println("Welcome to the Console Tester application");
-		console.println();
+		ConsoleIO c = IOFactory.getUserIO();
 
-		// int test block
-		console.println("Int Test");
-		console.getInt("Enter an integer between -100 and 100:  ", -101, 101);
+		c.println("Welcome to the Console Tester application");
+		c.println();
 
-		// double test block
-		console.println("Double Test");
-		console.getInt("Enter any number between -100 and 100:  ", -101, 101);
+		c.println("Int Test");
+		c.getInt("Enter an integer between -100 and 100:  ", -101, 101);
 
-		// String test block
-		console.println("Required String Test");
-		console.getString("Enter your email address:\n");
+		c.println("Double Test");
+		c.getInt("Enter any number between -100 and 100:  ", -101, 101);
 
-		// String choice test block
-		console.println("String Choice Test");
-		console.getString("Select one (x/y):\n", "x", "y");
+		c.println("Required String Test");
+		c.getString("Enter your email address:\n");
+
+		c.println("String Choice Test");
+		c.getString("Select one (x/y):\n", "x", "y");
 
 	}
 
